ec21115731c7fe3fa271da07d23fd05ee2bd532a
pedrojunior <ph751153@gmail.com>
2024-02-01 21:20:29 -0300

    Script de conversão adicionado

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function somaVolts(){
-    var re = parseInt(document.getElementById("RE"));
-    var co = parseInt(document.getElementById("AM"));
+    var re = parseInt(document.getElementById("revo").value);
+    var co = parseInt(document.getElementById("amvo").value);
 
     var volts = re * co;
 
-    document.getElementById("resulta").value = volts;
+    document.getElementById("volt").value = volts;
 }
