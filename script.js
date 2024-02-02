bcd42d58f701f70da175d3517aaa90bca4fd0eb1
pedrojunior <ph751153@gmail.com>
2024-02-02 18:29:46 -0300

    Calcula de amperes adicionado no script

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function somaVolts(){
 
     document.getElementById("volt").value = volts;
 }
+
+function calc_Amper(){
+    var vo = parseInt(document.getElementById("inpvo").value);
+    var re =  parseInt(document.getElementById("inpre").value);
+
+    var corrente =  vo / re;
+
+    document.getElementById("inpam").value = parseInt(corrente);
+}
